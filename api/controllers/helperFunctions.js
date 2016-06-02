ce36e1f5c4f0d321b8147099e4e89e7ea92f3581
Abhiram <abhiram.m@bookmyshow.com>
2016-06-02 17:32:36 +0530

    tiny tiny fixes

--- api/controllers/helperFunctions.js
+++ api/controllers/helperFunctions.js
@@ -4,12 +4,12 @@ var config=require("../../config").config;
 exports.generateId=function(collectionName,callback) {
     
     mango.connect(config.db, function(err, db) {
-        if(err){callback(false,err)}
+        if(err){callback(err,null)}
         var collection = db.collection("collectionsConfig");
         var cdn = {"_id":collectionName}; 
         
         collection.findAndModify(cdn,[], {$inc:{record_intCount:1}}, {new:true},function(err,doc){            
-            if(err){callback(false,err)}
+            if(err){callback(err,null)}
             
             var charsLength=doc.value.record_strPrefix.length+(doc.value.record_intCount).toString().length
             var zeros="";
